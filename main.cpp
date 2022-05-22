84d75290c8dc0a3bb339ddbeeef8bd0284eb5ec7
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2022-05-22 18:12:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
-#include <time.h>
+#include <ctime>
+#include <windows.h>
+
 using namespace sf;
 
-const int M = 20; // высота игрового поля
-const int N = 10; // ширина игрового поля
+const int M = 20; // РІС‹СЃРѕС‚Р° РёРіСЂРѕРІРѕРіРѕ РїРѕР»СЏ
+const int N = 10; // С€РёСЂРёРЅР° РёРіСЂРѕРІРѕРіРѕ РїРѕР»СЏ
 
-int field[M][N] = { 0 }; // игровое поле
+int field[M][N] = { 0 }; // РёРіСЂРѕРІРѕРµ РїРѕР»Рµ
 
-// Массив фигурок-тетрамино
-int figures[7][4] =
+
+int figures[7][4] = // РњР°СЃСЃРёРІ С„РёРіСѓСЂРѕРє-С‚РµС‚СЂР°РјРёРЅРѕ
 {
 	1,3,5,7, // I
 	2,4,5,7, // Z
@@ -23,9 +25,9 @@ int figures[7][4] =
 struct Point
 {
 	int x, y;
-}a[4], b[4];
+}a[4], b[4]; // С‚РѕС‡РєР° РЅР° РїРѕР»Рµ + 2 РІСЃРїРѕРјРѕРіР°С‚РµР»СЊРЅС‹С… РјР°СЃСЃРёРІР°
 
-// Проверка на выход за границы игрового поля
+// РџСЂРѕРІРµСЂРєР° РЅР° РІС‹С…РѕРґ Р·Р° РіСЂР°РЅРёС†С‹ РёРіСЂРѕРІРѕРіРѕ РїРѕР»СЏ
 bool check()
 {
 	for (int i = 0; i < 4; i++)
@@ -40,7 +42,7 @@ bool endGame(RenderWindow *window)
 {
 	for (int i = 0; i < N; i++)
 	{
-		if (field[2][i]) // замість 0нада 1 так правильніше
+		if (field[2][i]) 
 		{
 			window->close();
 			return false;
@@ -48,184 +50,219 @@ bool endGame(RenderWindow *window)
 	}
 }
 
+void Hide()
+{
+	HWND Hide;
+	AllocConsole();
+	Hide = FindWindowA("ConsoleWindowClass", NULL);
+	ShowWindow(Hide, 0);
+}
+
 int main()
 {
-	srand(time(0));
+	while (true) {
+		srand(time(0));
+		Hide();
 
-	RenderWindow window(VideoMode(320, 480), "Tetris"); // 320, 480
+		RenderWindow window(VideoMode(320, 480), "Tetris"); // 320, 480
 
-	// Создание и загрузка текстуры
-	Texture texture, texture_background, texture_frame, texture_game_over;
-	texture.loadFromFile("tiles.png");
-	texture_background.loadFromFile("background.png");
-	texture_frame.loadFromFile("frame.png");
-	texture_game_over.loadFromFile("game_over(1).png");
+		// РЎРѕР·РґР°РЅРёРµ Рё Р·Р°РіСЂСѓР·РєР° С‚РµРєСЃС‚СѓСЂС‹
+		Texture texture, texture_background, texture_frame, texture_game_over;
+		texture.loadFromFile("tiles.png");
+		texture_background.loadFromFile("background.png");
+		texture_frame.loadFromFile("frame.png");
+		texture_game_over.loadFromFile("game_over.png");
 
-	// Создание спрайта
-	Sprite sprite(texture), sprite_background(texture_background), sprite_frame(texture_frame), sprite_game_over(texture_game_over);
+		// РЎРѕР·РґР°РЅРёРµ СЃРїСЂР°Р№С‚Р°
+		Sprite sprite(texture), sprite_background(texture_background), sprite_frame(texture_frame), sprite_game_over(texture_game_over);
 
-	// Вырезаем из спрайта отдельный квадратик размером 18х18 пикселей
-	sprite.setTextureRect(IntRect(0, 0, 18, 18));
+		// Р’С‹СЂРµР·Р°РµРј РёР· СЃРїСЂР°Р№С‚Р° РѕС‚РґРµР»СЊРЅС‹Р№ РєРІР°РґСЂР°С‚РёРє СЂР°Р·РјРµСЂРѕРј 18С…18 РїРёРєСЃРµР»РµР№
+		sprite.setTextureRect(IntRect(0, 0, 18, 18));
 
-	// Переменные для горизонтального перемещения и вращения
-	int dx = 0; bool rotate = 0; int colorNum = 1; bool beginGame = true; int n = rand() % 7;
+		// РџРµСЂРµРјРµРЅРЅС‹Рµ РґР»СЏ РіРѕСЂРёР·РѕРЅС‚Р°Р»СЊРЅРѕРіРѕ РїРµСЂРµРјРµС‰РµРЅРёСЏ Рё РІСЂР°С‰РµРЅРёСЏ
+		int dx = 0; bool rotate = 0; int colorNum = 1; bool beginGame = true; int n = rand() % 7;
 
-	// Переменные для таймера и задержки
-	float timer = 0, delay = 0.3;
+		// РџРµСЂРµРјРµРЅРЅС‹Рµ РґР»СЏ С‚Р°Р№РјРµСЂР° Рё Р·Р°РґРµСЂР¶РєРё
+		float timer = 0, delay = 0.4;
 
-	// Часы (таймер)
-	Clock clock;
+		// Р§Р°СЃС‹ (С‚Р°Р№РјРµСЂ)
+		Clock clock;
 
+		bool IsRun = endGame(&window);
 
-	// Главный цикл приложения. Выполняется, пока открыто окно
-	while (window.isOpen() && endGame(&window))
-	{
-		// Получаем время, прошедшее с начала отсчета, и конвертируем его в секунды
-		float time = clock.getElapsedTime().asSeconds();
-		clock.restart();
-		timer += time;
-
-		// Обрабатываем очередь событий в цикле
-		Event event;
-		while (window.pollEvent(event))
+		// Р¦РёРєР» РёРіСЂС‹. Р’С‹РїРѕР»РЅСЏРµС‚СЃСЏ, РїРѕРєР° РѕС‚РєСЂС‹С‚Рѕ РѕРєРЅРѕ
+		while (window.isOpen() && IsRun)
 		{
-			
-			// Пользователь нажал на «крестик» и хочет окно закрыть?
-			if (event.type == Event::Closed)
-				// тогда закрываем его
-				window.close();
-
-			// Была нажата кнопка на клавиатуре?
-			if (event.type == Event::KeyPressed)
-				// Эта кнопка – стрелка вверх?…
-				if (event.key.code == Keyboard::Up) rotate = true;
-			// …или же стрелка влево?…
-				else if (event.key.code == Keyboard::Left) dx = -1;
-			// …ну тогда может это стрелка вправо?
-				else if (event.key.code == Keyboard::Right) dx = 1;
-		}
-
-		// Нажали кнопку "вниз"? Ускоряем падение тетрамино
-		if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
-
-		//// Горизонтальное перемещение ////
-		for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
-
-		// Вышли за пределы поля после перемещения? Тогда возвращаем старые координаты 
-		if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
-
-
-		//// Вращение ////
-		if (rotate)
-		{
-			Point p = a[1]; // задаем центр вращения
-			for (int i = 0; i < 4; i++)
+			IsRun = endGame(&window);
+			if (!IsRun)
 			{
-				int x = a[i].y - p.y; //y-y0
-				int y = a[i].x - p.x; //x-x0
-				a[i].x = p.x - x;
-				a[i].y = p.y + y;
+				break;
 			}
-			// Вышли за пределы поля после поворота? Тогда возвращаем старые координаты 
-			if (!check()) { for (int i = 0; i < 4; i++) a[i] = b[i]; }
 
-		}
+			// РџРѕР»СѓС‡Р°РµРј РІСЂРµРјСЏ, РїСЂРѕС€РµРґС€РµРµ СЃ РЅР°С‡Р°Р»Р° РѕС‚СЃС‡РµС‚Р°, Рё РєРѕРЅРІРµСЂС‚РёСЂСѓРµРј РµРіРѕ РІ СЃРµРєСѓРЅРґС‹
+			float time = clock.getElapsedTime().asSeconds();
+			clock.restart();
+			timer += time;
 
-		//// Движение тетрамино вниз (Тик таймера) ////
-		if (timer > delay)
-		{
-			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
-			if (!check())
+			// РћР±СЂР°Р±Р°С‚С‹РІР°РµРј РѕС‡РµСЂРµРґСЊ СЃРѕР±С‹С‚РёР№ РІ С†РёРєР»Рµ
+			Event event;
+			while (window.pollEvent(event))
 			{
-				for (int i = 0; i < 4; i++) field[b[i].y][b[i].x] = colorNum;
-				colorNum = 1 + rand() % 7;
+
+				// РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РЅР°Р¶Р°Р» РЅР° В«РєСЂРµСЃС‚РёРєВ» Рё С…РѕС‡РµС‚ РѕРєРЅРѕ Р·Р°РєСЂС‹С‚СЊ?
+				if (event.type == Event::Closed) {
+					// С‚РѕРіРґР° Р·Р°РєСЂС‹РІР°РµРј РµРіРѕ
+					window.close();
+					return 0;
+				}
+				// Р‘С‹Р»Р° РЅР°Р¶Р°С‚Р° РєРЅРѕРїРєР° РЅР° РєР»Р°РІРёР°С‚СѓСЂРµ?
+				if (event.type == Event::KeyPressed)
+					// Р­С‚Р° РєРЅРѕРїРєР° вЂ“ СЃС‚СЂРµР»РєР° РІРІРµСЂС…?вЂ¦
+					if (event.key.code == Keyboard::Up) rotate = true;
+				// вЂ¦РёР»Рё Р¶Рµ СЃС‚СЂРµР»РєР° РІР»РµРІРѕ?вЂ¦
+					else if (event.key.code == Keyboard::Left) dx = -1;
+				// вЂ¦РЅСѓ С‚РѕРіРґР° РјРѕР¶РµС‚ СЌС‚Рѕ СЃС‚СЂРµР»РєР° РІРїСЂР°РІРѕ?
+					else if (event.key.code == Keyboard::Right) dx = 1;
+			}
+
+			// РќР°Р¶Р°Р»Рё РєРЅРѕРїРєСѓ "РІРЅРёР·"? РЈСЃРєРѕСЂСЏРµРј РїР°РґРµРЅРёРµ С‚РµС‚СЂР°РјРёРЅРѕ
+			if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
+
+			//// Р“РѕСЂРёР·РѕРЅС‚Р°Р»СЊРЅРѕРµ РїРµСЂРµРјРµС‰РµРЅРёРµ ////
+			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
+
+			// Р’С‹С€Р»Рё Р·Р° РїСЂРµРґРµР»С‹ РїРѕР»СЏ РїРѕСЃР»Рµ РїРµСЂРµРјРµС‰РµРЅРёСЏ? РўРѕРіРґР° РІРѕР·РІСЂР°С‰Р°РµРј СЃС‚Р°СЂС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹ 
+			if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+
+
+			//// Р’СЂР°С‰РµРЅРёРµ ////
+			if (rotate)
+			{
+				Point p = a[1]; // Р·Р°РґР°РµРј С†РµРЅС‚СЂ РІСЂР°С‰РµРЅРёСЏ
+				for (int i = 0; i < 4; i++)
+				{
+					int x = a[i].y - p.y; //y-y0
+					int y = a[i].x - p.x; //x-x0
+					a[i].x = p.x - x;
+					a[i].y = p.y + y;
+				}
+				// Р’С‹С€Р»Рё Р·Р° РїСЂРµРґРµР»С‹ РїРѕР»СЏ РїРѕСЃР»Рµ РїРѕРІРѕСЂРѕС‚Р°? РўРѕРіРґР° РІРѕР·РІСЂР°С‰Р°РµРј СЃС‚Р°СЂС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹ 
+				if (!check()) { for (int i = 0; i < 4; i++) a[i] = b[i]; }
+
+			}
+
+			//// Р”РІРёР¶РµРЅРёРµ С‚РµС‚СЂР°РјРёРЅРѕ РІРЅРёР· (РўРёРє С‚Р°Р№РјРµСЂР°) ////
+			if (timer > delay)
+			{
+				for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
+				if (!check())
+				{
+					for (int i = 0; i < 4; i++) field[b[i].y][b[i].x] = colorNum;
+					colorNum = 1 + rand() % 7;
+					n = rand() % 7;
+					for (int i = 0; i < 4; i++)
+					{
+						a[i].x = figures[n][i] % 2;
+						a[i].y = figures[n][i] / 2;
+					}
+
+				}
+				timer = 0;
+
+			}
+
+			//----РџР РћР’Р•Р РљРђ Р›РРќРР----//
+			int k = M - 1;
+			for (int i = M - 1; i > 0; i--)
+			{
+				int count = 0;
+				for (int j = 0; j < N; j++)
+				{
+					if (field[i][j]) count++;
+					field[k][j] = field[i][j];
+				}
+				if (count < N) k--;
+			}
+
+			// РџРµСЂРІРѕРµ РїРѕСЏРІР»РµРЅРёРµ С‚РµС‚СЂР°РјРёРЅРѕ РЅР° РїРѕР»Рµ?
+			if (beginGame)
+			{
+				beginGame = false;
 				n = rand() % 7;
 				for (int i = 0; i < 4; i++)
 				{
 					a[i].x = figures[n][i] % 2;
 					a[i].y = figures[n][i] / 2;
 				}
-
 			}
-			timer = 0;
+			dx = 0; rotate = 0; delay = 0.3;
 
-		}
+			//----РћРўР РРЎРћР’РљРђ----//
 
-		//----ПРОВЕРКА ЛИНИИ----//
-		int k = M - 1;
-		for (int i = M - 1; i > 0; i--)
-		{
-			int count = 0;
-			for (int j = 0; j < N; j++)
-			{
-				if (field[i][j]) count++;
-				field[k][j] = field[i][j];
-			}
-			if (count < N) k--;
-		}
+			// Р—Р°РґР°РµРј С†РІРµС‚ С„РѕРЅР° - Р±РµР»С‹Р№
+			window.clear(Color::White);
+			window.draw(sprite_background);
+			for (int i = 0; i < M; i++)
+				for (int j = 0; j < N; j++)
+				{
+					if (field[i][j] == 0) continue;
+					sprite.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+					sprite.setPosition(j * 18, i * 18);
+					sprite.move(28, 31); // СЃРјРµС‰РµРЅРёРµ
+					window.draw(sprite);
+				}
 
-		// Первое появление тетрамино на поле?
-		if (beginGame)
-		{
-			beginGame = false;
-			n = rand() % 7;
 			for (int i = 0; i < 4; i++)
 			{
-				a[i].x = figures[n][i] % 2;
-				a[i].y = figures[n][i] / 2;
-			}
-		}
-		dx = 0; rotate = 0; delay = 0.3;
+				// Р Р°Р·СѓРєСЂР°С€РёРІР°РµРј С‚РµС‚СЂР°РјРёРЅРѕ
+				sprite.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
 
-		//----ОТРИСОВКА----//
+				// РЈСЃС‚Р°РЅР°РІР»РёРІР°РµРј РїРѕР·РёС†РёСЋ РєР°Р¶РґРѕРіРѕ РєСѓСЃРѕС‡РєР° С‚РµС‚СЂР°РјРёРЅРѕ
+				sprite.setPosition(a[i].x * 18, a[i].y * 18);
 
-		// Задаем цвет фона - белый
-		window.clear(Color::White);
-		window.draw(sprite_background);
-		for (int i = 0; i < M; i++)
-			for (int j = 0; j < N; j++)
-			{
-				if (field[i][j] == 0) continue;
-				sprite.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
-				sprite.setPosition(j * 18, i * 18);
-				sprite.move(28, 31); // смещение
+				sprite.move(28, 31); // СЃРјРµС‰РµРЅРёРµ
+
+				// РћС‚СЂРёСЃРѕРІРєР° СЃРїСЂР°Р№С‚Р°
 				window.draw(sprite);
 			}
+			// РћС‚СЂРёСЃРѕРІРєР° С„СЂРµР№РјР°
+			window.draw(sprite_frame);
 
-		for (int i = 0; i < 4; i++)
-		{
-			// Разукрашиваем тетрамино
-			sprite.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
-
-			// Устанавливаем позицию каждого кусочка тетрамино
-			sprite.setPosition(a[i].x * 18, a[i].y * 18);
-
-			sprite.move(28, 31); // смещение
-
-			// Отрисовка спрайта
-			window.draw(sprite);
+			// РћС‚СЂРёСЃРѕРІРєР° РѕРєРЅР°
+			window.display();
 		}
-		// Отрисовка фрейма
-		window.draw(sprite_frame);
 
-		// Отрисовка окна
-		window.display();
-	}
-
-	RenderWindow window2(VideoMode(320, 480), "Tetris");
-
-	while (window2.isOpen())
-	{
-		Event event;
-		while (window2.pollEvent(event))
+		IsRun = true;
+		
+		for (int i = 0; i < M; i++)   
 		{
-			// Пользователь нажал на «крестик» и хочет закрыть окно?
-			if (event.type == Event::Closed) window2.close();
+			for (int j = 0; j < N; j++)   
+			{
+				field[i][j] = 0;
+			}
 		}
-		window2.clear(Color::White);
-		window2.draw(sprite_game_over);
-		window2.display();
-	}
 
-	return 0;
+		RenderWindow window2(VideoMode(320, 480), "Tetris");
+
+		while (window2.isOpen())
+		{
+			Event event;
+			while (window2.pollEvent(event))
+			{
+				// РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РЅР°Р¶Р°Р» РЅР° В«РєСЂРµСЃС‚РёРєВ» Рё С…РѕС‡РµС‚ Р·Р°РєСЂС‹С‚СЊ РѕРєРЅРѕ?
+				if (event.type == Event::Closed)
+				{
+					window2.close();
+					return 0;
+				}
+			}
+			window2.clear(Color::White);
+			window2.draw(sprite_game_over);
+			window2.display();
+			if (Keyboard::isKeyPressed(Keyboard::Space))
+			{
+				break;
+			}
+		}
+	}
 }
